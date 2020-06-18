21a90a96357376a21f8a63537c824065e3dd8582
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-06-18 16:51:10 +0200

    fixed bugs

--- src/pages/Feed/HomeClient.js
+++ src/pages/Feed/HomeClient.js
@@ -1,115 +1,113 @@
-import React, {useEffect} from "react"
-import { ChatFeed, Message } from 'react-chat-ui'
-import { useState } from "react"
-import { TextField, Button } from "@material-ui/core"
+import React, { useEffect } from "react";
+import { ChatFeed, Message } from "react-chat-ui";
+import { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
 import openSocket from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:8080";
 
-
-export default function HomeClient({userId, token}) {
-  const [newMessage, setNewMessage] = useState("")
+export default function HomeClient({ userId, token }) {
+  const [newMessage, setNewMessage] = useState("");
   const sendMessage = (message) => {
-    setMessages([...messages,new Message({ id: 0, message: text })]);
-    fetch('http://localhost:8080/chat/'+userId,{
-      method: 'POST',
-      body:JSON.stringify({
-        message
+    fetch("http://localhost:8080/chat/" + userId, {
+      method: "POST",
+      body: JSON.stringify({
+        message,
       }),
       headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer '+ token
-        },
-  })
-    .then(res => {
-      if (res.status !== 200) {
-        throw new Error('Failed to post status');
-      }
-      return res.json();
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
     })
-    .then(resData => {
-      console.log("resDat",resData.chat)
-    })
-    .catch(err => {
-      console.log(err);
-    });
-  } 
-
-  useEffect (() => {
-    setMessages([...messages,newMessage]);
-  },[newMessage])
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error("Failed to post status");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log("resDat", resData.chat);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   useEffect(() => {
-  var socket = (openSocket(ENDPOINT));
-  socket.on("new message", data => {
-    setNewMessage(new Message({ id: data.isAdmin? 1:0, 
-      message: data.message }));
-  })
+    setMessages([...messages, newMessage]);
+  }, [newMessage]);
 
-  fetch('http://localhost:8080/chat/'+userId,{
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer '+ token
+  useEffect(() => {
+    var socket = openSocket(ENDPOINT);
+    socket.emit("join room", userId);
+    socket.on("new message", (data) => {
+      setNewMessage(
+        new Message({ id: data.isAdmin ? 1 : 0, message: data.message })
+      );
+    });
+
+    fetch("http://localhost:8080/chat/" + userId, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
       },
-})
-  .then(res => {
-    if (res.status !== 200) {
-      throw new Error('Failed to fetch status');
-    }
-    return res.json();
-  })
-  .then(resData => {
-    console.log("resDat",resData.chat)
-    setMessages(parseData(resData.chat))
-  })
-  .catch(err => {
-    console.log(err);
-  });
+    })
+      .then((res) => {
+        if (res.status !== 200) {
+          throw new Error("Failed to fetch status");
+        }
+        return res.json();
+      })
+      .then((resData) => {
+        console.log("resDat", resData.chat);
+        setMessages(parseData(resData.chat));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
 
-  const parseData = (data) =>{
-    return data.reverse().map(msg=>{
+  const parseData = (data) => {
+    return data.reverse().map((msg) => {
       return new Message({
-        id: msg.isAdmin&& msg.isAdmin ? 1:0,
-        message: msg.message && msg.message
-      })
-  })
-   
-  }
+        id: msg.isAdmin && msg.isAdmin ? 1 : 0,
+        message: msg.message && msg.message,
+      });
+    });
+  };
 
-  const [text,setText]= useState("");
+  const [text, setText] = useState("");
 
-  const [messages,setMessages]= useState([])
+  const [messages, setMessages] = useState([]);
   return (
-  <div><ChatFeed
-      messages={messages} // Boolean: list of message objects
-      isTyping={false} // Boolean: is the recipient typing
-      hasInputField={false} // Boolean: use our input, or use your own
-      showSenderName // show the name of the user who sent the message
-      bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
-      // JSON: Custom bubble styles
-      bubbleStyles={
-        {
+    <div>
+      <ChatFeed
+        messages={messages} // Boolean: list of message objects
+        isTyping={false} // Boolean: is the recipient typing
+        hasInputField={false} // Boolean: use our input, or use your own
+        showSenderName // show the name of the user who sent the message
+        bubblesCentered={false} //Boolean should the bubbles be centered in the feed?
+        // JSON: Custom bubble styles
+        bubbleStyles={{
           text: {
-            fontSize: 30
+            fontSize: 30,
           },
           chatbubble: {
             borderRadius: 70,
-            padding: 40
-          }
-        }
-      }
-    />
-    <TextField onChange={event =>setText(event.target.value)}></TextField>
-  <Button
-    variant="contained" 
-    color="primary" 
-    onClick={()=>{
-      sendMessage(text)
-    }}>
-    Send
-  </Button>
+            padding: 40,
+          },
+        }}
+      />
+      <TextField onChange={(event) => setText(event.target.value)}></TextField>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          sendMessage(text);
+        }}
+      >
+        Send
+      </Button>
     </div>
-    )
-
+  );
 }
--- src/pages/Feed/Table.js
+++ src/pages/Feed/Table.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import { Button } from '@material-ui/core';
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles({
   table: {
@@ -15,57 +15,75 @@ const useStyles = makeStyles({
   },
 });
 
-export default function ClientTable({setId,page,token,setFalseAdmin}) {
+export default function ClientTable({ onLeave, page, token, setFalseAdmin }) {
   const classes = useStyles();
-  const [rows,setRows] = useState([])
+  const [rows, setRows] = useState([]);
 
-  useEffect(()=>{
-    fetch('http://localhost:8080/clients/?page='+page,{
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer '+ token
-          },
+  useEffect(() => {
+    fetch("http://localhost:8080/clients/?page=" + page, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
     })
-      .then(res => {
+      .then((res) => {
         if (res.status !== 200) {
-            setFalseAdmin();
-          throw new Error('Failed to fetch status');
+          setFalseAdmin();
+          throw new Error("Failed to fetch status");
         }
         return res.json();
       })
-      .then(resData => {
-        setRows(resData.clients)
+      .then((resData) => {
+        setRows(resData.clients);
       })
-      .catch(err => {
+      .catch((err) => {
         setFalseAdmin();
         console.log(err);
       });
-  },[page])
+  }, [page]);
 
-
-  if (rows.length == 0 ) return null
+  if (rows.length == 0) return null;
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="simple table">
         <TableHead>
-          <TableRow style={{background:"purple"}}>
-          <TableCell style={{color:"white"}} align="center"></TableCell>
-            <TableCell style={{color:"white"}} align="center">E- mail</TableCell>
-            <TableCell style={{color:"white"}} align="center">Username</TableCell>
-            <TableCell style={{color:"white"}} align="center">OS Version</TableCell>
-            <TableCell style={{color:"white"}} align="center">App Version</TableCell>
-            <TableCell style={{color:"white"}} align="center">Status</TableCell>
-            <TableCell style={{color:"white"}} align="center">Device Language</TableCell>
+          <TableRow style={{ background: "purple" }}>
+            <TableCell style={{ color: "white" }} align="center"></TableCell>
+            <TableCell style={{ color: "white" }} align="center">
+              E- mail
+            </TableCell>
+            <TableCell style={{ color: "white" }} align="center">
+              Username
+            </TableCell>
+            <TableCell style={{ color: "white" }} align="center">
+              OS Version
+            </TableCell>
+            <TableCell style={{ color: "white" }} align="center">
+              App Version
+            </TableCell>
+            <TableCell style={{ color: "white" }} align="center">
+              Status
+            </TableCell>
+            <TableCell style={{ color: "white" }} align="center">
+              Device Language
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.email}>
-                   <TableCell align="center"> <Button variant="contained" 
-                    onClick={()=> setId(row._id)} color="primary"  style={{margin:20}}  >
-    Chat
-    </Button></TableCell>
+              <TableCell align="center">
+                {" "}
+                <Button
+                  variant="contained"
+                  onClick={() => onLeave(row._id)}
+                  color="primary"
+                  style={{ margin: 20 }}
+                >
+                  Chat
+                </Button>
+              </TableCell>
               <TableCell align="center">{row.email}</TableCell>
               <TableCell align="center">{row.username}</TableCell>
               <TableCell align="center">{row.osname}</TableCell>
